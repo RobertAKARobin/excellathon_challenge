459720988a6919af622ed79ca6127e439fa1e283
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-30 09:34:39 -0500

    Added FizzBuzz

--- server.js
+++ server.js
@@ -16,6 +16,17 @@ app.post("/round0", function(req, res){
   res.json(out);
 });
 
+app.post("/fizzbuzz", function(req, res){
+  var nums = req.body, out = [];
+  nums.forEach(function(num){
+    if(num % 6 === 0) out.push("FizzBuzz");
+    else if(num % 2 === 0) out.push("Fizz");
+    else if(num % 3 === 0) out.push("Buzz");
+    else out.push(num);
+  });
+  res.json(out);
+});
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("Listening!");
 });

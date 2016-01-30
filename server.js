a4e0f42329fc5c7f220f9fd58ae6eea6dd32d568
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-30 09:23:52 -0500

    Round 0

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.post("/round0", function(req, res){
+  var i = 0, out = [];
+  for(i = 0; i < req.body; i++){
+    out.push("Hello World");
+  }
+  res.json(out);
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Listening!");
+});
